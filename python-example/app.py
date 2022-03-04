4d0c5bcd79abd5ad0f4d73eaa5b5187360f1da1b
Jeremiah Carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2022-03-04 09:06:13 -0500

    Update app.py

--- python-example/app.py
+++ python-example/app.py
@@ -3,10 +3,10 @@
 import numpy as np
 import pandas as pd
 import os
-from pathlib import path
+from pathlib import Path
 
 localDir = Path(os.path.dirname(os.path.realpath(__file__))) # This files absolute path
 
-df = pd.read_csv(localDir/'dataTarget'/'data.csv') 
+df = pd.read_csv(localDir/'dataTarget'/'data.csv', header=None) 
 
 df.apply(np.sum, axis=1).to_csv(localDir/'dataOut'/'data.csv')
